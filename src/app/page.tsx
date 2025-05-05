444d3fa89cca7b428a7d5db37412433dbdd76abf
benitavo <ribeaudb38@gmail.com>
2025-05-05 13:00:18 +0100

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-blue-200 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-gray-800">Hello Tailwind!</h1>
+    </main>
+  );
+}
